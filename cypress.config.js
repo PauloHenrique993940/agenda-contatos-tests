4c31b44cde671cc8390170773b127b5ebe418c19
Paulo Henrique Ferreira França <paulohenriqueferreirafranca2@gmail.com>
2025-03-06 18:28:46 -0300

    Corrigindo  alterando contatos

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorio',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: 'ddmmyyyy_HHMMss'
+
+  }
 });
--- cypress/e2e/2-advanced-examples/agenda.cy.js
+++ cypress/e2e/2-advanced-examples/agenda.cy.js
@@ -1,45 +1,51 @@
 /// <reference types="cypress"/>
 
 describe('Testando Funcionalidades', () => {
-    beforeEach(()=>{
-      cy.visit('https://agenda-contatos-react.vercel.app/')
-    })
-  
-    it('Adicionando Contatos', () => {
-      cy.get('[type="text"]').type("Joao")
-      cy.get('[type="email"]').type("joao@teste.com")
-      cy.get('[type="tel"]').type("62 9 99999999")
-      cy.get('.adicionar').click()
-  
-      cy.get('[type="text"]').type("Maria")
-      cy.get('[type="email"]').type("maria@teste.com")
-      cy.get('[type="tel"]').type("62 9 88888888")
-      cy.get('.adicionar').click()
-  
-      cy.get('[type="text"]').type("Pedro")
-      cy.get('[type="email"]').type("pedro@teste.com")
-      cy.get('[type="tel"]').type("62 9 77777777")
-      cy.get('.adicionar').click()
-    })
-  
-    it('Alterando Contatos', () => {
-      cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
-      cy.get('[type="text"]').clear().type("Joao Barbosa")
-      cy.get('.ckeKmo button.alterar').click()
-  
-      cy.wait(500);
-      cy.get(':nth-child(3) > .sc-gueYoa > .edit').click()
-      cy.get('[type="text"]').clear().type("Maria Fernanda")
-      cy.get('.ckeKmo button.alterar').click()
-  
-      cy.wait(500);
-      cy.get(':nth-child(4) > .sc-gueYoa > .edit').click()
-      cy.get('[type="text"]').clear().type("Pedro Augusto")
-      cy.get('.ckeKmo button.alterar').click()
-    })
-  
-    it('Removendo um contato', () => {
-      cy.get(':nth-child(4) > .sc-gueYoa > .delete').click()
-    })
+  beforeEach(() => {
+    cy.visit('https://agenda-contatos-react.vercel.app/')
   })
+
+  it('Adicionando Contatos', () => {
+    cy.get('[type="text"]').type("Joao")
+    cy.get('[type="email"]').type("joao@teste.com")
+    cy.get('[type="tel"]').type("62 9 99999999")
+    cy.get('.adicionar').click()
+
+    cy.get('[type="text"]').type("Maria")
+    cy.get('[type="email"]').type("maria@teste.com")
+    cy.get('[type="tel"]').type("62 9 88888888")
+    cy.get('.adicionar').click()
+
+    cy.get('[type="text"]').type("Pedro")
+    cy.get('[type="email"]').type("pedro@teste.com")
+    cy.get('[type="tel"]').type("62 9 77777777")
+    cy.get('.adicionar').click()
+  })
+
+  it('Alterando Contatos', () => {
+    cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
+    cy.get('[type="text"]').clear().type("Joao Barbosa")
+    cy.get('[type="email"]').clear().type("joaobarbosa@teste.com")
+    cy.get('[type="tel"]').clear().type("62 9 99999999")
+    cy.get('.ckeKmo button.alterar').click()
+  
+    cy.wait(500);
+    cy.get(':nth-child(3) > .sc-gueYoa > .edit').click()
+    cy.get('[type="text"]').clear().type("Maria Fernanda")
+    cy.get('[type="email"]').clear().type("mariafernanda@teste.com")
+    cy.get('[type="tel"]').clear().type("62 9 88888888")
+    cy.get('.ckeKmo button.alterar').click()
+  
+    cy.wait(500);
+    cy.get(':nth-child(4) > .sc-gueYoa > .edit').click()
+    cy.get('[type="text"]').clear().type("Pedro Augusto")
+    cy.get('[type="email"]').clear().type("pedroaugusto@teste.com")
+    cy.get('[type="tel"]').clear().type("62 9 77777777")
+    cy.get('.ckeKmo button.alterar').click()
+  })
+  
+  it('Removendo um contato', () => {
+    cy.get(':nth-child(4) > .sc-gueYoa > .delete').click()
+  })
+})
 
